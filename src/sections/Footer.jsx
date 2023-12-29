40ba4a1e81e66ba13765b662c0176f68c80cb417
swadebsharma6 <swadebriva2012@gmail.com>
2023-12-30 03:20:50 +0600

    added footer component

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,10 +1,18 @@
+import { footerLogo } from "../assets/images";
 
 
 const Footer = () => {
     return (
-        <div>
-          Footer  
-        </div>
+        <footer className="max-container">
+         <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+          <div className="flex flex-col items-start">
+          <a href="/">
+          <img src={footerLogo} alt="logo" width={150} height={46} />
+          </a>
+         <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">Ensuring premium comfort and style, Our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+          </div>
+         </div>
+        </footer>
     );
 };
 
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,10 +1,21 @@
+import Button from "../components/Button";
 
 
 const Subscribe = () => {
     return (
-        <div>
-            Subscribe
-        </div>
+        <section
+        id="contact-us"
+        className="max-container flex justify-between items-center max-lg:flex-col gap-10">
+           <h3 className="text-4xl leading-[68px] lg:max-w-md font-bold font-palanquin">Sign Up form 
+           <span className="text-coral-red"> Updates</span> & Newsletter
+           </h3>
+           <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded">
+            <input type="text" placeholder="subscribe@nike.com" className="input" />  
+            <div>
+            <Button label='Sign Up'></Button>
+            </div>       
+            </div>
+        </section>
     );
 };
 
