e9fe92172459be24ea4f15df46ac14bdf5d42f10
swadebsharma6 <swadebriva2012@gmail.com>
2023-12-30 02:39:46 +0600

    added special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,10 @@
 import { FaArrowRight } from "react-icons/fa6";
 
-const Button = ({label}) => {
+const Button = ({label, backGroundColor, textColor}) => {
     return (
-       <button className="flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+       <button className={`flex justify-center items-center gap-2 px-7 py-4 border text-lg leading-none 
+       ${backGroundColor ? `${backGroundColor} ${textColor}`  : 'bg-coral-red rounded-full text-white border-coral-red '}
+       `}>
       {label}
       <FaArrowRight />
        </button>
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,10 +1,25 @@
+import { offer } from "../assets/images";
+import Button from "../components/Button";
 
 
 const SpecialOffer = () => {
     return (
-        <div>
-            Offer
-        </div>
+        <section className="flex justify-wrap text-center max-xl:flex-col-reverse gap-10 max-container" >
+            <div className="flex-1">
+            <img src={offer} alt=""  width={773} height={687} className="object-contain w-full"/>
+            </div>
+            <div className="flex flex-1 flex-col">
+            <h2 className=" font-palanquin capitalize text-4xl font-bold lg:max-w-lg">
+            <span className="text-coral-red inline-block mt-3"> Special </span>  Offer
+            </h2>
+            <p className="mt-4 lg:max-w-lg info-text">Ensuring premium comfort and style, Our meticulously crafted footwear is designed to elevate your experience, providing you with unmatched quality, innovation, and a touch of elegance.</p>
+            <p className="mt-4 info-text lg:max-w-lg mb-4">Our dedication to detail and excellence ensure your satisfaction</p>
+            <div className="mt-11 flex flex-wrap gap-4">
+            <Button label='Show Now'></Button>
+            <Button label='Learn More' backGroundColor='bg-white' textColor='text-slate-gray'></Button>
+            </div>
+            </div>
+        </section>
     );
 };
 
